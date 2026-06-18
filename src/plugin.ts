9a330e9ee5327468945251eb42c5c051cf917ce0
timvdhoorn <timvdhoorn@gmail.com>
2026-06-18 15:50:41 +0200

    feat: wire store + actions into plugin entry point

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,6 +1,25 @@
+// src/plugin.ts
 import streamDeck from "@elgato/streamdeck";
+import { createHerdrClient } from "./herdr/client";
+import { createAgentStore } from "./core/store";
+import { AgentSlotAction } from "./actions/slot";
+import { PagerAction } from "./actions/pager";
 
-// Placeholder entry point created during scaffolding.
-// The real wiring (store + actions) is implemented in the final task.
 streamDeck.logger.setLevel("info");
+
+const herdr = createHerdrClient();
+const store = createAgentStore({ fetchAgents: () => herdr.listAgents() });
+
+const slot = new AgentSlotAction(store, herdr);
+const pager = new PagerAction(store);
+
+store.subscribe(() => {
+  slot.renderAll();
+  pager.renderAll();
+});
+
+streamDeck.actions.registerAction(slot);
+streamDeck.actions.registerAction(pager);
+
 streamDeck.connect();
+store.start(1000);
